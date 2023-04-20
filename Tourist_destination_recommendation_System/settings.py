8eb16b570672c9986aefdde6a95c60c262bb2ddd
pranami <pranamiujjwal0000@gmail.com>
2023-04-20 16:07:50 +0530

    juyst

--- Tourist_destination_recommendation_System/settings.py
+++ Tourist_destination_recommendation_System/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-m1nztr_du74y^vxt1)xwk=vq+^edkoxotlnx9*crr-+m)61j@e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
